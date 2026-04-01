91c3c3a2f9cc0a956a76d9d560e33f351dd4a0e0
V1adis1av0 <ry84kovv@yandex.ru>
2026-04-01 20:25:20 +0300

    lab5. basic database connection

--- lab5/lab5_python.py
+++ lab5/lab5_python.py
@@ -1,2 +1,24 @@
 import psycopg2
 
+try:
+    conn = psycopg2.connect(
+        dbname="python_db",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432"
+    )
+
+    print("Успешное подключение!")
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT version();")
+    db_version = cur.fetchone()
+    print("Версия PostgreSQL:", db_version)
+
+    cur.close()
+    conn.close()
+
+except Exception as e:
+    print("Ошибка:", e)
